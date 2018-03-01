843cd78cf7ff225aa21bfe5823a175ef2cbed068
Irwin <IrwiStu18@issaquah.wednet.edu>
2018-03-01 15:52:28 -0800

    Added few more ties

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,11 +1,11 @@
 function show(id, press) {
 	var div = document.getElementById(id);
 
-	if(press.value == "Show") {
-		div.style.display = "flex";
-		press.value = "Hide";
-	} else {
+	if(div.style.display == "flex") {
 		div.style.display = "none";
 		press.value = "Show";
+	} else {
+		div.style.display = "flex";
+		press.value = "Hide";
 	}
 }
